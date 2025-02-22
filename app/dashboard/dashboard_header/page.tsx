7ecb19dff746a17d02c8f74e29d7c16790122ffa
Crispus <crispus.nzano@gmail.com>
2025-02-22 18:32:30 +0200

    fix: remove sidebartrigger

--- app/dashboard/dashboard_header/page.tsx
+++ app/dashboard/dashboard_header/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { SidebarTrigger } from "@/components/ui/sidebar"
 import AppBar from "@/components/layout/AppBar";
 
 export default function DashboardHeader() {
@@ -9,7 +8,6 @@ export default function DashboardHeader() {
 
       <div className="flex items-center">
           <AppBar/>
-            <SidebarTrigger />
         <h1 className="ml-4 text-2xl font-bold">Church Database Dashboard</h1>
       </div>
     </header>
